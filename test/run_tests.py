f7c637dadf51d6e8e8cc1a63d4488c72f8fee6c5
3rd Iteration <stevekr@gmail.com>
2025-10-06 21:53:57 -0400

    Normalize bech32 decoder results under MicroPython

--- test/run_tests.py
+++ test/run_tests.py
@@ -22,26 +22,22 @@ if is_micropython:
     try:
         import bech32 as _bech32_module  # type: ignore
 
-        def _wrap_decode_if_needed(attr):
+        def _ensure_pair_result(attr):
             func = getattr(_bech32_module, attr, None)
             if func is None:
                 return
-            try:
-                result = func('bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kg3g4ty')
-            except Exception:
-                return
-            if isinstance(result, tuple) and len(result) >= 2 and len(result) != 2:
-                def _decoder(*args, **kwargs):
-                    res = func(*args, **kwargs)
-                    if isinstance(res, tuple) and len(res) >= 2:
-                        return res[0], res[1]
-                    return res
 
-                setattr(_bech32_module, attr, _decoder)
+            def _decoder(*args, **kwargs):
+                res = func(*args, **kwargs)
+                if isinstance(res, tuple) and len(res) > 2:
+                    return res[0], res[1]
+                return res
 
-        _wrap_decode_if_needed('decode')
-        _wrap_decode_if_needed('bech32_decode')
-        _wrap_decode_if_needed('bech32m_decode')
+            setattr(_bech32_module, attr, _decoder)
+
+        _ensure_pair_result('decode')
+        _ensure_pair_result('bech32_decode')
+        _ensure_pair_result('bech32m_decode')
     except ImportError:  # pragma: no cover - MicroPython without bundled bech32
         pass
 else:
